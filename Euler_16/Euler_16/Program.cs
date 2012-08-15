d0d610ad35908f091e624875f57691a9202f6c74
Mark <markgray.online@googlemail.com>
2012-08-15 15:32:37 +0100

    Completed Euler Problem 16

---
+++ Euler_16/Euler_16/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace Euler_16
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			new Action(() =>
+			{
+				Console.WriteLine(BigInteger
+					.Pow(2, 1000)
+					.ToString()
+					.Aggregate<char, double>(0, (current1, c) => current1 + (int)Char.GetNumericValue(c)));
+				Console.ReadKey();
+			}).Invoke();
+		}
+	}
+}
