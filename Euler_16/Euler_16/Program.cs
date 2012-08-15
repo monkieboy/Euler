52f25f602b5a70f2d9d9bcf98c6dba14fe08158b
Mark <markgray.online@googlemail.com>
2012-08-15 16:41:22 +0100

    Added another technique without BigInteger.

--- Euler_16/Euler_16/Program.cs
+++ Euler_16/Euler_16/Program.cs
@@ -11,25 +11,61 @@ namespace Euler_16
 		{
 
 			new EulerProcessor().DoIt(() => BigInteger
-			                                    .Pow(2, 1000)
-			                                    .ToString()
-			                                    .Aggregate(0,
-			                                                (total, next) => total + (int) Char.GetNumericValue(next)));
+												.Pow(2, 1000)
+												.ToString()
+												.Aggregate(0,
+															(total, next) => total + (int)Char.GetNumericValue(next)));
 
 			new EulerProcessor().DoIt(() =>
-			                          	{
-			                          		var result = 0;
+										{
+											var result = 0;
 
-			                          		var number = BigInteger.Pow(2, 1000);
+											var number = BigInteger.Pow(2, 1000);
 
-			                          		while (number > 0)
-			                          		{
-			                          			result += (int) (number%10);
-			                          			number /= 10;
-			                          		}
+											while (number > 0)
+											{
+												result += (int)(number % 10);
+												number /= 10;
+											}
 
-			                          		return result;
-			                          	});
+											return result;
+										});
+
+			new EulerProcessor().DoIt(() =>
+			{
+				int sum = 0;
+
+				int[] arr = new int[400];
+
+				arr[0] = 2;
+				int carry = 0;
+
+				for (int j = 1; j < 1000; j++)
+				{
+					for (int i = 0; i < arr.Length; i++)
+					{
+						int tempno = arr[i] * 2;
+
+						if (tempno > 9)
+						{
+							arr[i] = (tempno % 10) + carry;
+							carry = 1;
+						}
+						else
+						{
+							arr[i] = tempno + carry;
+							carry = 0;
+						}
+					}
+				}
+
+				for (int i = 0; i < arr.Length; i++)
+				{
+					sum += arr[i];
+				}
+
+				return sum;
+			});
 			Console.ReadKey();
 		}
 	}
