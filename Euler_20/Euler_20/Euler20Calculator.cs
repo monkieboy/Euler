0896abde5608da0638c7a535b507ae8af647b248
Mark Gray <markgray.online@googlemail.com>
2012-08-19 21:28:11 +0100

    Solved Euler problem 20

---
+++ Euler_20/Euler_20/Euler20Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System.Numerics;
+
+namespace Euler_20
+{
+    public class Euler20Calculator
+    {
+        public BigInteger GetMultipleCalculation(int seed)
+        {
+            var result = new BigInteger(1);
+            for (int i = 2; i <= seed; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+
+        public BigInteger GetSumCalculation(BigInteger seed)
+        {
+           var sum = new BigInteger(0);
+
+            while (seed > 0)
+            {
+                sum += seed % 10;
+                seed /= 10;
+            }
+
+           return sum;
+
+        }
+    }
+}
---
+++ Euler_20/Euler_20_tests/Euler_20_tests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Numerics;
+using Euler_20;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Euler_20_tests
+{
+    [TestFixture]
+    public class Euler_20_tests
+    {
+        private BigInteger result;
+        private Euler20Calculator euler20Calculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            result = 1;
+            euler20Calculator = new Euler20Calculator();
+        }
+
+        [Test]
+        public void Should_calculate_the_composite_integer_multiples_of_10_to_3628800()
+        {
+            result = euler20Calculator.GetMultipleCalculation(10);
+
+            result.Should().Be(3628800);
+        }
+
+        [Test]
+        public void Should_calculate_the_composite_integer_sum()
+        {
+            result = 3628800;
+
+            var sum = euler20Calculator.GetSumCalculation(result);
+
+            sum.Should().Be(27);
+
+        }
+
+        [Test]
+        public void Should_get_the_answer_for_euler_20()
+        {
+            const int seed = 100;
+
+            Console.WriteLine(euler20Calculator.GetSumCalculation(euler20Calculator.GetMultipleCalculation(seed)));
+        }
+    }
+}
