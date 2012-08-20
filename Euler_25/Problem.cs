3c64533ace0280be525da8d7886d0c8430d63ea4
Mark Gray <markgray.online@googlemail.com>
2012-08-20 22:47:33 +0100

    Completed Euler 25

---
+++ Euler_25/Problem.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+namespace Euler_25
+{
+    public class Problem
+    {
+        private readonly List<BigInteger> sequence = new List<BigInteger>();
+
+        public Problem()
+        {
+            Seed = 1;
+            Start = 1;
+        }
+
+        public int Start { get; set; }
+
+        public int Seed { get; set; }
+        
+        public BigInteger GetFibonacciElementFor(int i)
+        {
+            GenerateFibonacciUpToLimit(i);
+
+            return sequence[i-1];
+        }
+
+        public IList<BigInteger> GenerateTestSequence()
+        {
+            GenerateFibonacciUpToLimit(10);
+
+            return sequence;
+        }
+
+        private void GenerateFibonacciUpToLimit(int limit)
+        {
+            var count = 1;
+            var current = 1;
+            var next = 1;
+            sequence.Add(1);
+            sequence.Add(1);
+            while (count <= limit)
+            {
+                var temp = current + next;
+                current = next;
+                next = temp;
+                sequence.Add(temp);
+                count++;
+            }
+        }
+
+        public BigInteger LocateFirstFibonacciWithNumberGreaterThan(BigInteger limit)
+        {
+            BigInteger current = 1;
+            BigInteger next = 1;
+            int count = 2;
+            while (next <= limit)
+            {
+                current = current + next;
+                count+=2;
+                next = current + next;
+            }
+            return count;
+        }
+    }
+}
---
+++ Euler_25_tests/Tests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using Euler_25;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Euler_25_tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        private Problem problem = new Problem();
+
+        [Test]
+        public void Should_generate_Fibonacci_sequences()
+        {
+            var generateTestSequence = problem.GenerateTestSequence();
+            generateTestSequence.Should().ContainInOrder(new BigInteger[] { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 });
+            generateTestSequence.ToList().ForEach(x => Console.Write("{0} ", x));
+
+        }
+
+        [Test]
+        public void The_seventh_element_in_the_Fibonacci_sequence_is_13()
+        {
+            problem.GetFibonacciElementFor(7).Should().Be(13);
+        }
+
+        [Test]
+        public void The_twelth_element_in_the_Fibonacci_sequence_is_144()
+        {
+            problem.GetFibonacciElementFor(12).Should().Be(144);
+        }
+
+        [Test]
+        public void Get_the_first_fibonacci_with_3_digits()
+        {
+            var number = problem.LocateFirstFibonacciWithNumberGreaterThan(100);
+
+            number.Should().Be(12);
+        }
+
+        [Test]
+        public void Get_the_first_fibonacci_with_2_digits()
+        {
+            var number = problem.LocateFirstFibonacciWithNumberGreaterThan(10);
+
+            number.Should().Be(8);
+        }
+
+        [Test]
+        public void Get_the_first_fibonacci_with_over_10_digits()
+        {
+            var number = problem.LocateFirstFibonacciWithNumberGreaterThan(BigInteger.Pow(10, 9));
+
+            number.Should().BeGreaterOrEqualTo(46);
+
+            Console.WriteLine(number);
+        }
+
+        [Test]
+        public void Get_the_first_fibonacci_with_over_1000_digits()
+        {
+            var bigInteger = BigInteger.Pow(10, 999);
+            var number = problem.LocateFirstFibonacciWithNumberGreaterThan(bigInteger);
+
+            Console.WriteLine(number);
+        }
+    }
+}
