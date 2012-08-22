9f70b25cb1ff61ce1c98581804bef2513fbea865
Mark Gray <markgray.online@googlemail.com>
2012-08-22 23:50:56 +0100

    Started Euler 17

---
+++ Euler_17/Problem.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+
+namespace Euler_17
+{
+    public class Problem
+    {
+        public string GetWordForNumber(int number)
+        {
+            return Change.ToString(number);
+        }
+    }
+
+    public class Change
+    {
+        public static string ToString(int number)
+        {
+            return number.Convert();
+        }
+    }
+
+    public static class ChangeExtensions
+    {
+        private static readonly Dictionary<int, string> NumberWords = new Dictionary<int, string>
+                                                                 {
+                                                                     {1, "one "},
+                                                                     {2, "two "},
+                                                                     {3, "three "},
+                                                                     {4, "four "},
+                                                                     {5, "five "},
+                                                                     {6, "six "},
+                                                                     {7, "seven "},
+                                                                     {8, "eight "},
+                                                                     {9, "nine "},
+                                                                     {10, "ten "},
+                                                                     {11, "eleven "},
+                                                                     {12, "twelve "},
+                                                                     {13, "thirteen "},
+                                                                     {14, "fourteen "},
+                                                                     {15, "fifteen "},
+                                                                     {16, "sixteen "},
+                                                                     {17, "seventeen "},
+                                                                     {18, "eighteen "},
+                                                                     {19, "nineteen "},
+                                                                     {20, "twenty "},
+                                                                     {30, "thirty "},
+                                                                     {40, "forty "},
+                                                                     {50, "fifty "},
+                                                                     {60, "sixty "},
+                                                                     {70, "seventy "},
+                                                                     {80, "eighty "},
+                                                                     {90, "ninety "},
+                                                                     {100, "hundred "},
+                                                                     {1000, "thousand "},
+                                                                 };
+
+        public static string Convert(this int number)
+        {
+            string result = string.Empty;
+            int iteration = 0;
+            while (number >= 1)
+            {
+                //Figure out the current number
+
+                // 16/5 = 3 r 1
+                var tenth = number % 10;
+                number /= 10;
+                if (tenth != 0)
+                {
+                    if (iteration == 1)
+                    {
+                        result = NumberWords[tenth * 10] + result;
+                    }
+                    else
+                    {
+                if (iteration == 3 && tenth > 0 && !result.Contains("and"))
+                {
+                    result = "thousand and " + result;
+                }
+                else if (iteration == 3 && tenth == 0 && !result.Contains("and"))
+                {
+                    result = "and " + result;
+                }
+                else if (iteration == 3 && tenth > 0 && result.Contains("and"))
+                {
+                    result = "thousand " + result;
+                }
+                if (iteration == 2 && tenth > 0)
+                {
+                    result = "hundred and " + result;
+                }
+                else if (iteration == 2 && tenth == 0)
+                {
+                    result = "and " + result;
+                }
+                        result = NumberWords[tenth] + result;
+                    }
+                }
+
+                //if (iteration)
+
+                iteration++;
+            }
+            return result.TrimEnd(new[] { ' ' });
+        }
+    }
+}
---
+++ Euler_17_tests/Euler_17_tests.cs
@@ -0,0 +1,47 @@
+﻿using Euler_17;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Euler_17_tests
+{
+    [TestFixture]
+    public class Euler_17_tests
+    {
+        [Test]
+        public void Should_return_the_word_one_when_supplied_with_1()
+        {
+            var p = new Problem();
+
+            p.GetWordForNumber(1).Should().Be("one");
+        }
+
+        [Test]
+        public void Should_return_the_words_one_hundred_and_one_when_supplied_with_101()
+        {
+            var p = new Problem();
+
+            p.GetWordForNumber(101).Should().Be("one hundred and one");
+        }
+        [Test]
+        public void Should_return_the_words_two_hundred_and_one_when_supplied_with_201()
+        {
+            var p = new Problem();
+
+            p.GetWordForNumber(201).Should().Be("two hundred and one");
+        }
+        [Test]
+        public void Should_return_the_words_three_hundred_and_fifty_four_when_supplied_with_354()
+        {
+            var p = new Problem();
+
+            p.GetWordForNumber(354).Should().Be("three hundred and fifty four");
+        }
+        [Test]
+        public void Should_return_the_words_one_thousand_three_hundred_and_fifty_four_when_supplied_with_1354()
+        {
+            var p = new Problem();
+
+            p.GetWordForNumber(1354).Should().Be("one thousand three hundred and fifty four");
+        }
+    }
+}
